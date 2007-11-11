9e29e8657216657c9c5153435985f157c5872ac0
fredericback <fredericback@c41c4367-7422-0410-acad-89ad0d30c555>
2007-11-11 20:07:01 +0000

    * Patch by Steve: Make spacings in options dialogue more gnome-y.
    git-svn-id: http://geditclassbrowser.googlecode.com/svn/trunk@41 c41c4367-7422-0410-acad-89ad0d30c555

--- classbrowser/options.py
+++ classbrowser/options.py
@@ -86,6 +86,7 @@ class Options(gobject.GObject):
         win = gtk.Window()
         win.connect("delete-event",lambda w,e: w.destroy())
         win.set_title("Preferences")
+        win.set_border_width(6)
         vbox = gtk.VBox() 
 
         #--------------------------------  
@@ -94,8 +95,8 @@ class Options(gobject.GObject):
         notebook.set_border_width(6)
         vbox.pack_start(notebook)
 
-        vbox2 = gtk.VBox()
-        vbox2.set_border_width(6) 
+        vbox2 = gtk.VBox(spacing=6)
+        vbox2.set_border_width(6)
 
         box = gtk.HBox()
         verbose = gtk.CheckButton("show debug information")
@@ -118,16 +119,16 @@ class Options(gobject.GObject):
         notebook.append_page(vbox2,gtk.Label("General"))
 
         #--------------------------------       
-        vbox2 = gtk.VBox()
+        vbox2 = gtk.VBox(spacing=6)
         vbox2.set_border_width(6)
 
         button = {}
         for i in self.colours:
-            box = gtk.HBox()
+            box = gtk.HBox(spacing=6)
             button[i] = gtk.ColorButton()
             button[i].set_color(self.colours[i])
-            box.pack_start(button[i],False)
-            box.pack_start(gtk.Label(i),False,False,6)
+            box.pack_end(button[i],False)
+            box.pack_start(gtk.Label(i.capitalize() + " :"), False, False, 6)
             vbox2.pack_start(box)
 
         notebook.append_page(vbox2,gtk.Label("Colours"))
@@ -154,13 +155,16 @@ class Options(gobject.GObject):
             self.emit("options-changed")
             win.destroy()
 
-        box = gtk.HBox()
-        b = gtk.Button(None,gtk.STOCK_OK)
-        b.connect("clicked",setValues)
-        box.pack_end(b,False)
+        box = gtk.HButtonBox()
+        box.set_spacing(6)
+        box.set_border_width(6)
+        box.set_layout(gtk.BUTTONBOX_END)
         b = gtk.Button(None,gtk.STOCK_CANCEL)
         b.connect("clicked",lambda w,win: win.destroy(),win)
-        box.pack_end(b,False)
+        box.add(b)
+        b = gtk.Button(None,gtk.STOCK_OK)
+        b.connect("clicked",setValues)
+        box.add(b)
         vbox.pack_start(box,False)
 
         win.add(vbox)
